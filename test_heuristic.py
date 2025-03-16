91b396b0bbde57c010276c32cbabfae331e3c2cd
edzya <edgars.zoltners@gmail.com>
2025-03-16 10:05:48 +0200

    Mistake

--- test_heuristic.py
+++ test_heuristic.py
@@ -2,17 +2,17 @@ from heuristic import Heuristic
 
 def test_heuristic():
     test_cases = [
-        (5, 0),   # Sāk ar 5 punktiem, jānovērtē labākais gājiens
-        (13, 0),  # Sāk ar 10 punktiem, spēlētājam ir 3 punkti
-        (15, -2), # Sāk ar 15 punktiem, spēlētājam ir -2 punkti
-        (20, 5),  # Sāk ar 20 punktiem, spēlētājam ir 5 punkti
+        (5),   # Sāk ar 5 punktiem, jānovērtē labākais gājiens
+        (13),  # Sāk ar 10 punktiem, spēlētājam ir 3 punkti
+        (15), # Sāk ar 15 punktiem, spēlētājam ir -2 punkti
+        (20),  # Sāk ar 20 punktiem, spēlētājam ir 5 punkti
     ]
 
-    for current_number, player_score in test_cases:
+    for current_number in test_cases:
         score = Heuristic.evaluate(current_number)[0]
         move = Heuristic.evaluate(current_number)[1]
-        print(f"Skaitlis: {current_number}, Punkti: {player_score} -> Heuristikas vērtējums: {score}")
-        print(f"Skaitlis: {current_number}, Punkti: {player_score} -> Izvēlētais skaitlis: {move}")
+        print(f"Skaitlis: {current_number}-> Heuristikas vērtējums: {score}")
+        print(f"Skaitlis: {current_number}-> Izvēlētais skaitlis: {move}")
 
 # Palaiž testu
 test_heuristic()
